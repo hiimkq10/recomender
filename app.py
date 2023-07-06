79332e1a8d44cafbba7137e8dd30e0b0f8ef43c7
Ke Quang <trankequang9221@gmail.com>
2023-07-06 23:08:49 +0700

    Add load model and predict

--- app.py
+++ app.py
@@ -1,23 +1,44 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 import tensorflow as tf
 import os
+import random
 
 app = Flask(__name__)
 port = int(os.getenv('PORT', 5000))
 
-incomes = [
-    { 'description': 'salary', 'amount': 5400 }
-]
-
-@app.route('/')
+@app.route('/recommend', methods=['POST'])
 def index():
+    # Get json input
+    data = request.json
+    if (data['ids'] is None):
+        return {
+            'status': 10001,
+            'message': "Data invalid"
+        }
+
+    # String array to byte array
+    input = []
+    for i in data['ids']:
+        if (i.strip()):
+            input.append(tf.compat.as_bytes(i))
+    n = 30
+    fill = [b'0'] * n
+    input = input[:n] + fill[len(input):]
+    random.shuffle(input)
+    print(input)
+
+    # Load model and predict 
     model = tf.saved_model.load("")
-    scores, titles = model([[b'436', b'155', b'163', b'437', b'237', b'453', b'160', b'435',
-      b'152', b'440', b'26', b'130', b'235', b'158', b'152', b'145',
-      b'113', b'233', b'146', b'16', b'15', b'86', b'164', b'444',
-      b'128', b'113', b'435', b'112', b'440', b'227']])
-    print(titles[0][:3])
-    return "Hello"
+    _, titles = model([input])
+
+    # Byte array to string array
+    predict_arr = []
+    for i in tf.keras.backend.get_value(titles[0][:20]):
+        predict_arr.append(tf.compat.as_str_any(i))
+    result = {
+        'ids':  predict_arr
+    }
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=port)
